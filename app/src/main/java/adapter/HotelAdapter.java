256ef1b0a32d77d8913d9579cb3ac4f74d1a0772
Grandis Rufaidah <grandisrufaidah99@gmail.com>
2017-01-03 09:34:40 -0800

    RecyclerView Edit.

---
+++ app/src/main/java/adapter/HotelAdapter.java
@@ -0,0 +1,121 @@
+package adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.learn.recyclerview3.R;
+import model.Hotel;
+
+/**
+ * Created by Smktelkomm on 11/15/2016.
+ */
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> {
+    IHotelAdapter mIhotelAdapter;
+    public interface IHotelAdapter
+    {
+        void doClick(int pos);
+        void doEdit(int pos);
+        void doDelete(int pos);
+        void doFav(int pos);
+        void doShare(int pos);
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    ArrayList<Hotel> hotelList;
+
+    public HotelAdapter(Context context, ArrayList<Hotel> hotelList)
+    {
+        this.hotelList = hotelList;
+        mIhotelAdapter = (IHotelAdapter) context;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Hotel hotel = hotelList.get(position);
+        holder.tvJudul.setText(hotel.judul);
+        holder.tvDeskripsi.setText(hotel.deskripsi);
+        holder.ivFoto.setImageURI(Uri.parse(hotel.foto));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (hotelList!=null)
+            return hotelList.size();
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivFoto;
+        TextView tvJudul;
+        TextView tvDeskripsi;
+        Button bEdit;
+        Button bDelete;
+        ImageButton ibFav;
+        ImageButton ibShare;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
+            tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
+            tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+            bEdit = (Button) itemView.findViewById(R.id.buttonEdit);
+            bDelete = (Button) itemView.findViewById(R.id.buttonDelete);
+            ibFav = (ImageButton) itemView.findViewById(R.id.buttonFavorite);
+            ibShare = (ImageButton) itemView.findViewById(R.id.buttonShare);
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIhotelAdapter.doClick(getAdapterPosition());
+                }
+
+            });
+            bEdit.setOnClickListener(new View.OnClickListener()
+            {
+
+                @Override
+                public void onClick(View view) {
+                    mIhotelAdapter.doEdit(getAdapterPosition());
+                }
+            });
+            bDelete.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+                    mIhotelAdapter.doDelete(getAdapterPosition());
+                }
+            });
+            ibFav.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                     mIhotelAdapter.doFav(getAdapterPosition());
+                }
+            });
+            ibShare.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        mIhotelAdapter.doShare(getAdapterPosition());
+                }
+            });
+
+        }
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -24,8 +24,11 @@ import model.Hotel;
 public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
     public static final String HOTEL = "hotel";
     public static final int REQUEST_CODE_ADD = 88;
+    public static final int REQUEST_CODE_EDIT = 99;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
+    int itemPos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +62,13 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK) {
             Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
             mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        }
+        else if (requestCode == REQUEST_CODE_EDIT && requestCode == RESULT_OK)
+        {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.remove(itemPos);
+            mList.add(itemPos, hotel);
             mAdapter.notifyDataSetChanged();
         }
     }
@@ -118,5 +128,29 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(HOTEL, mList.get(pos));
         startActivity(intent);
+    }
+
+    @Override
+    public void doEdit(int pos)
+    {
+        itemPos = pos;
+        Intent intent = new Intent(this, InputActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
+        startActivityForResult(intent, REQUEST_CODE_EDIT);
+    }
+
+    @Override
+    public void doDelete(int pos) {
+
+    }
+
+    @Override
+    public void doFav(int pos) {
+
+    }
+
+    @Override
+    public void doShare(int pos) {
+
     }
 }
